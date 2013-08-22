f2f53f096e73f412b305f75e8fe939b27c172840
Jose Gonzalez <josecarlosgonz@gmail.com>
2013-08-21 23:48:51 -0400

    Updated fror hw4
    Created dev branch too

---
+++ homeworkSolutions/hw4.sh
@@ -0,0 +1,24 @@
+#! usr/bin/bash
+
+### HW4 solutions
+## use crl-u to clear bash line
+
+git clone https://github.com/pepecarlos/bitstarter
+emacs web.js # change port to 8080
+npm install   # node package manager
+node web.js # listening
+
+#heroku app
+http://secure-spire-2911.herokuapp.com
+
+ec2-54-213-147-78.us-west-2.compute.amazonaws.com:8080  # specify port after url
+
+
+### Review screens lecture
+
+screen #start screen interface
+
+crl-t ? # go to help, hit space to exit
+crt-t j or ctrl-t u or ctrl-t numbers or ctrl-t crl-t #switch back and forth between tabs
+crl-t crl-c #create a new tab
+crl-d #exit a screen
